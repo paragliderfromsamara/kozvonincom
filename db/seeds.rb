853f5b6919b622e33748a8b0cbb540d72d3d7001
Roman Kozvonin <romankozvonin@gmail.com>
2016-10-10 00:01:00 +0400

    09-10-2016 - seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(ru_name: "Роман Козвонин", en_name: "Roman Kozvonin", password: "123456", password_confirmation: "123456", email: "romankozvonin@gmail.com", vk_url: '', fb_url: '', user_type_id: 1)
+User.create(ru_name: "Роман Козвонин", com_name: "Roman Kozvonin", password: "123456", password_confirmation: "123456", email: "romankozvonin@gmail.com", vk_url: '', fb_url: '', user_type_id: 1)
 Category.create(
 [
   {
     ru_name: "Природа",
-    en_name: "Nature",
+    com_name: "Nature",
     ru_description: '',
-    en_description: '',
+    com_description: '',
     order_number: 1,
     is_enable: true
   },
   {
     ru_name: "Аэрофото",
-    en_name: "Aerophoto",
+    com_name: "Aerophoto",
     ru_description: '',
-    en_description: '',
+    com_description: '',
     order_number: 2,
     is_enable: true
   },
     {
       ru_name: "Путешествия",
-      en_name: "Trips",
+      com_name: "Trips",
       ru_description: '',
-      en_description: '',
+      com_description: '',
       order_number: 3,
       is_enable: true
     },
     {
       ru_name: "Самара",
-      en_name: "Samara",
+      com_name: "Samara",
       ru_description: '',
-      en_description: '',
+      com_description: '',
       order_number: 4,
       is_enable: true
     }
